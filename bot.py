0bad58d9b3c3646f11b50194e6099cdf1b0d07d2
thatboytrevvv <trevortracy0106@gmail.com>
2026-04-11 23:00:10 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,27 @@
 import discord
-import asyncio
-from datetime import datetime
-import pytz
 import os
-
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+import asyncio
+ 
 TOKEN = os.environ["DISCORD_TOKEN"]
 CHANNEL_ID = 1491178939983331459
-
+ 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
-
+scheduler = AsyncIOScheduler(timezone="America/New_York")
+ 
 async def send_morning_message():
-    await client.wait_until_ready()
     channel = client.get_channel(CHANNEL_ID)
-    tz = pytz.timezone("America/New_York")
-
-    while not client.is_closed():
-        now = datetime.now(tz)
-        if now.weekday() == 0 and now.hour == 9 and now.minute == 0:
-            await channel.send("@everyone Good morning! ☀️ Let's have a nice and productive week & make a bunch of money 💲 so we can spend it all on SKINS!")
-            await asyncio.sleep(60)
-        await asyncio.sleep(30)
-
+    if channel:
+        await channel.send("@everyone Good morning! ☀️ Let's have a nice and productive week & make a bunch of money 💲 so we can spend it all on SKINS!")
+    else:
+        print("Channel not found!")
+ 
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user}")
-    client.loop.create_task(send_morning_message())
-
+    scheduler.add_job(send_morning_message, CronTrigger(day_of_week="mon", hour=9, minute=0))
+    scheduler.start()
+ 
 client.run(TOKEN)
